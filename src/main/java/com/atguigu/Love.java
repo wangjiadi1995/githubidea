1ed60bbd1b518398b7f7035efc6d8c84380b6200
王嘉棣 <478345933@qq.com>
2020-09-26 21:34:08 +0800

    演示合并分支产生冲突

--- src/main/java/com/atguigu/Love.java
+++ src/main/java/com/atguigu/Love.java
@@ -3,6 +3,6 @@ package com.atguigu;
 public class Love {
     public static void main(String[] args) {
         System.out.println("你好世界");
-        System.out.println("你也好");
+        System.out.println("你也好,晚安陌生人");
     }
 }
